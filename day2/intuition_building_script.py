e61cd0bf7605bf55ded92bba4b7dabc39a1dc1a6
Snitkoff-Sol <151076247+Snitkoff-Sol@users.noreply.github.com>
2026-08-18 11:01:54 +0300

    Update

--- day2/intuition_building_script.py
+++ day2/intuition_building_script.py
@@ -24,19 +24,19 @@ sim = ElectroKitty(mechanism)
 sim.create_simulation(kin, cell_const, D, iso, [0.0001, 10, 10**-5, 0], intial_conditions)
 
 #defining ACV experiment
-J = 100
-dt = 0.002
+J = 1000
+dt = 0.005
+fs=1/dt
 
 Ei = 0.5
 Ef = -0.5
-# v = 0.1
+
 nt = 2**14
 amp = 0.1
-
-freq = 9
-
-v = abs(Ei - Ef)*freq/J
+freq = J*fs/nt
+v = 2*abs(Ei - Ef)*fs/nt
 print(v)
+print(freq)
 
 sim.V_potential(Ei, Ef, v, amp, freq, nt)
 
@@ -47,57 +47,63 @@ sim.Plot_simulation()
 
 #Harmonic extraction
 
+
+######------This is Electrokitty's functions------#########
 #sim.FFT_analyze_sim(freq, 10, band*np.ones(12))
 #sim.Harmonic_plots(plot_sim=True,w=1)
 #sim.FT_plot()
 
+#####------Let's start by doing the FFT----####
 
-n_h = 7
-
-j_fft = np.fft.fft(j)
+j_fft = np.fft.fft(j) # FT the current
 
 l = len(t)
 
-harmonic = np.zeros((l, n_h))
+fs = len(t) / t[-1] # our sampling frequency 
 
-fs = len(t) / t[-1]
+x_f = np.fft.fftfreq(len(t), d=t[1] - t[0]) # the frequencies
 
-band = 5
-
-
-x_f = np.fft.fftfreq(len(t), d=t[1] - t[0])
 plt.plot(x_f,np.abs(j_fft))
 
 
+#----Parameters for the harmonic extraction------
+band = 5 
+harmonic = np.zeros((l, n_h)) #Pre-alocate space
+N_harmonics = 7 # Define the number of harmonics to extract
 #std = 7
-
-
 #g = sig.windows.gaussian(l, std)
 
-for i in range(n_h):
-    h = i + 1
-    
+
+#----This loop extraction the harmonics------
+for i in range(N_harmonics):
+    h = i + 1 # Starts from fundamental up to 8
+
+    # We defind the upper and lower bounds of the bandpass filter
     f_low = h * freq - band / 2  
     f_high = h * freq + band / 2  
     
    
     #mask = (x_f >= f_low) & (x_f <= f_high)
+    
+    # This is a rectangular bandbass
     mask = (
         ((x_f >= f_low) & (x_f <= f_high)) |
         ((x_f >= -f_high) & (x_f <= -f_low))
     )
+
+    # This convolutes a gaussian with the rectangular to reduce side oscillations
     #conv_mask = sig.convolve(mask, g, mode='same')
     #norm_mask = conv_mask / np.max(conv_mask)
     
    
     #j_fft_masked = j_fft * norm_mask
+    # This is where the filter is applied
     j_fft_masked = j_fft * mask
     
- 
+    # FT back to the time domain
     j_ifft = np.fft.ifft(j_fft_masked)
     
-    
-    
+    # The FT gives us
     harmonic[:, i] = np.abs(sig.hilbert(np.real(j_ifft)))
 
 dc_mask = np.abs(x_f) <= band
